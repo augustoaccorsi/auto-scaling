5849da5500e63381bc1be892bdaecc31c993b080
augustoaccorsi <augusto.acorsi@gmail.com>
2021-04-06 00:19:43 -0300

    add event loop to run tasks asynchronous

--- run.py
+++ run.py
@@ -12,20 +12,26 @@ class Run:
     def __init__(self):        
         #self._app = App(auto_scaling_group, region, accessKeyId, secretAccessKey, sessionToken)   
         self._app = App("engine-asg", "sa-east-1")
-        self._localApp = App("engine-asg", "sa-east-1") 
+        self._localApp = App("engine-asg", "sa-east-1")     
 
     async def auto_scaling_check(self):
+        count = 0
         while True:
-            print("Executing Analysis on Auto Scaling Group "+auto_scaling_group)
+            count +=1
+            print("Executing Analysis "+str(count)+" on Auto Scaling Group "+auto_scaling_group)
+            print("-----")
             self._app.create_files()
             self._app.read_instances()
             print("Analysis Completed")
             print("----------------------------------")
-            await asyncio.sleep(20)
+            await asyncio.sleep(60)
 
     async def auto_scaling_check_local(self):
+        count = 0
         while True:
-            print("Executing Analysis on Auto Scaling Group "+"engine-asg")
+            count +=1
+            print("Executing Analysis "+str(count)+" on Auto Scaling Group "+"engine-asg")
+            print("-----")
             self._localApp.create_files()
             self._localApp.read_instances()
             print("Analysis Completed")
@@ -35,9 +41,9 @@ class Run:
 if __name__ == '__main__':
     run = Run()
     if env == 'dev':
-        print("----------------------------------")
         loop = asyncio.get_event_loop()
         try:
+            print("----------------------------------")
             asyncio.ensure_future(run.auto_scaling_check_local())
             loop.run_forever()
         except KeyboardInterrupt:
@@ -46,9 +52,9 @@ if __name__ == '__main__':
             print("Closing Loop")
             loop.close()
     else:
-        print("----------------------------------")
         loop = asyncio.get_event_loop()
         try:
+            print("----------------------------------")
             asyncio.ensure_future(run.auto_scaling_check())
             loop.run_forever()
         except KeyboardInterrupt:
