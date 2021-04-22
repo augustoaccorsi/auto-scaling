ba17d37543f031455716a47011f3d7c0b00a9c94
augustoaccorsi <augusto.acorsi@gmail.com>
2021-04-22 15:47:17 -0300

    update call

--- callxtimes.py
+++ callxtimes.py
@@ -10,7 +10,7 @@ from requests.packages.urllib3.util.retry import Retry
 class Run:
 
     def __init__(self):        
-        self._urlPost = "http://augusto-accorsi-engine-elb-1884692720.sa-east-1.elb.amazonaws.com:5000/engine/mand?max_iter=1100&width=1100&height=1100"
+        self._urlPost = "http://augusto-accorsi-engine-elb-1884692720.sa-east-1.elb.amazonaws.com:5000/engine/mand?max_iter=950&width=950&height=950"
         self._urlGet = "http://localhost:5000"
 
     def call_request_get(self, count):
@@ -24,10 +24,10 @@ class Run:
         return res.status_code
         
     async def call_url(self, type):
-        await asyncio.sleep(600)
+        #await asyncio.sleep(600)
         count = 0
         while True:
-            minutes = randint(10, 20)
+            minutes = randint(0, 20)
             sleep = (randint(10, 20) * 60)
             print("calls: "+str(minutes))
             print("sleep: "+str(sleep/60))
@@ -40,7 +40,7 @@ class Run:
                     status_code = self.call_request_get(count)
                 if status_code != 201:
                     exit_code+=1
-                if exit_code == 5:
+                if exit_code == 15:
                     exit_code = 0
                     break
                 count+=1
